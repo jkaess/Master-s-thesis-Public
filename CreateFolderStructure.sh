9b8863310631c8903abfda7ae40eb76a7aae0097
jkaess <j.kaessmaier@googlemail.com>
2025-11-20 17:16:24 +0100

    Provided a setup for creating the folderstructure

---
+++ CreateFolderStructure.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REPO_DIR=$(pwd)  # Get the current directory 
+
+SPEECHES_DIR="$REPO_DIR/../Speeches"  
+INDIVIDUAL_SPEECHES_DIR="$SPEECHES_DIR/IndividualSpeeches"  
+
+mkdir -p "$INDIVIDUAL_SPEECHES_DIR"
+
+echo "Folder structure created at: $INDIVIDUAL_SPEECHES_DIR"
+
